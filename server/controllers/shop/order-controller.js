9c482413b849a3ff64fd350935f831ff117ddcdd
Sujendran <sujendranjanagiraman@gmail.com>
2025-06-06 02:18:34 +0530

    new

--- server/controllers/shop/order-controller.js
+++ server/controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "http://localhost:5173/shop/paypal-return",
-        cancel_url: "http://localhost:5173/shop/paypal-cancel",
+        return_url: process.env.PAYPAL_RETURN_URL,
+        cancel_url: process.env.PAYPAL_CANCEL_URL,
       },
       transactions: [
         {
--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 
 paypal.configure({
   mode: "sandbox",
-  client_id: process.env.PAYPAL_CLIENT_ID,
-  client_secret: process.env.PAYPAL_CLIENT_SECRET,
+  client_id:
+    process.env.PAYPAL_CLIENT_ID || "http://localhost:5173/shop/paypal-return",
+  client_secret:
+    process.env.PAYPAL_CLIENT_SECRET ||
+    "http://localhost:5173/shop/paypal-cancel",
 });
 
 module.exports = paypal;
